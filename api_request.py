28dfc1cbd798e36dc857a5380b64176f76a6d83e
Tomek Lewandowski <tomeklewandowski570@gmail.com>
2022-02-24 12:10:13 +0100

    api_requests dataframe arrange method

--- api_request.py
+++ api_request.py
@@ -1,4 +1,8 @@
+from re import search
 from pytrends.request import TrendReq
+import gtab
+import matplotlib.pyplot as plt
+import pandas as pd
 
 class GoogleRequests:
     def __init__(self, keywords_arr, cat, timeframes, country_code, gprop):
@@ -7,9 +11,13 @@ class GoogleRequests:
         self.timeframes = timeframes
         self.country_code = country_code
         self.gprop = gprop
-        self.pytrends = TrendReq(hl='en-US', tz=360, timeout=(10,25), retries=2, backoff_factor=0.1)
         self.querry_arr = []
-    
+
+        self.pytrends = TrendReq(hl='en-US', tz=360, timeout=(10,25), retries=2, backoff_factor=0.1)
+        self.t = gtab.GTAB()
+        self.t.set_options(pytrends_config={"geo": country_code, "timeframe": "2021-01-01 2022-01-01"})
+        self.df = pd.DataFrame()
+
     def interest_over_time(self):
         self.pytrends.build_payload(self.keywords_arr, self.cat, self.timeframes[2], self.country_code, self.gprop)
         data = self.pytrends.interest_over_time()
@@ -50,3 +58,28 @@ class GoogleRequests:
     def search_array(self):
         self.related_queries()
         return [*self.keywords_arr, *self.querry_arr]
+    
+    def arrange_data(self, word_list):
+        self.pytrends.build_payload(self.keywords_arr, self.cat, self.timeframes[2], self.country_code, self.gprop)
+        #plt.figure()
+        ax = plt.subplot2grid((1, 1), (0, 0), rowspan=1, colspan=1)
+
+        for kw in word_list:
+            kw_query = self.t.new_query(kw)
+            self.df[kw] = kw_query.max_ratio
+            ax.plot(kw_query.max_ratio, label=kw)
+            ax.legend()
+    
+    def request_window(self):
+        request_arr = []
+        search_arr = self.search_array()
+        plt.figure()
+
+        for i in range(len(search_arr)):
+            request_arr.append(search_arr[i])
+            
+            if i % 5 == 0:
+                self.arrange_data(request_arr)
+                request_arr = []
+        
+        return self.df
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from re import search
+from urllib import request
+from django.http import QueryDict
 import pycountry
 from pytrends.request import TrendReq
 import gtab
@@ -28,46 +31,15 @@ TIMEFRAMES = ['today 12-m', 'today 3-m', 'today 1-m']
 GPROP = ''
 KW = []
 
-def arrange_data(country_code, word_list):
-    pytrends.build_payload(word_list, CAT, TIMEFRAMES[2], country_code, GPROP)
-    #plt.figure()
-    ax = plt.subplot2grid((1, 1), (0, 0), rowspan=1, colspan=1)
-
-    for kw in word_list:
-        kw_query = t.new_query(kw)
-        df[kw] = kw_query.max_ratio
-        ax.plot(kw_query.max_ratio, label=kw)
-        ax.legend()
-
-    return df
-
-def request_window(search_arr):
-    request_arr = []
-    plt.figure()
-
-    for i in range(len(search_arr)):
-        request_arr.append(search_arr[i])
-        
-        if i % 5 == 0:
-            arrange_data(country_code, request_arr)
-            request_arr = []
-
 def plot_data(data):
     plt.figure()
     df.plot()
+    print("shit should be plotted")
     plt.show()
-
-# SEARCH_ARR = [*KEYWORDS, *QUERY_ARR]
-# #plot_absolute_comparison(country_code, KEYWORDS)
-# # get_absolute_data(country_code)
-# request_window(SEARCH_ARR)
-# plot_data(df)
 
 google_requests = GoogleRequests(KEYWORDS, CAT, TIMEFRAMES, country_code, GPROP)
 # google_requests.interest_over_time()
 # google_requests.interest_per_region()
 # google_requests.country_trends()
-data = google_requests.search_array()
-#print(data)
-request_window(data)
-plot_data(data)
+data = google_requests.request_window()
+print(data)
