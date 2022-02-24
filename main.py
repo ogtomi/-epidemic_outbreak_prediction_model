64b604af169b5ff4e0eee5d10bc1600abed2cb77
Tomek Lewandowski <tomeklewandowski570@gmail.com>
2022-02-24 10:38:20 +0100

    Adding data to pandas dataframe

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from re import search
+from urllib import request
+from django.http import QueryDict
 import pycountry
 from pytrends.request import TrendReq
 import gtab
 import matplotlib.pyplot as plt
+import pandas as pd
 
 plt.style.use('ggplot')
 pytrends = TrendReq(hl='en-US', tz=360, timeout=(10,25), retries=2, backoff_factor=0.1)
@@ -9,14 +13,16 @@ pytrends = TrendReq(hl='en-US', tz=360, timeout=(10,25), retries=2, backoff_fact
 def country_to_code(country):
     return pycountry.countries.get(name=str(country)).alpha_2
 
-country = "poland"
+country = "united states"
 country_code = country_to_code(country)
 
 t = gtab.GTAB()
+df = pd.DataFrame()
 t.set_options(pytrends_config={"geo": country_code, "timeframe": "2021-01-01 2022-01-01"})
 #t.create_anchorbank(verbose=True)
 
-KEYWORDS = ["covid", 'pizza', 'porno']
+KEYWORDS = ['stomach']
+QUERY_ARR = []
 SCALING_KW_LIST = ["chicago", "poland", "warsaw", "gdansk"]
 CAT = '0'
 TIMEFRAMES = ['today 12-m', 'today 3-m', 'today 1-m']
@@ -28,26 +34,18 @@ def interest_over_time(country_code):
     data = pytrends.interest_over_time()
     return data
 
-def plot_absolute_comparison(country_code):
-    pytrends.build_payload(KEYWORDS, CAT, TIMEFRAMES[2], country_code, GPROP)
-    plt.figure()
+def arrange_data(country_code, word_list):
+    pytrends.build_payload(word_list, CAT, TIMEFRAMES[2], country_code, GPROP)
+    #plt.figure()
     ax = plt.subplot2grid((1, 1), (0, 0), rowspan=1, colspan=1)
 
-    for kw in KEYWORDS:
+    for kw in word_list:
         kw_query = t.new_query(kw)
+        df[kw] = kw_query.max_ratio
         ax.plot(kw_query.max_ratio, label=kw)
         ax.legend()
 
-    plt.show()
-
-def get_absolute_data(country_code):
-    pytrends.build_payload(KEYWORDS, CAT, TIMEFRAMES[2], country_code, GPROP)
-    
-    for kw in KEYWORDS:
-        kw_query = t.new_query(kw)
-        print(kw_query['max_ratio'][0])
-        print(kw_query['max_ratio'][1])
-        print(kw_query['max_ratio'][0] + kw_query['max_ratio'][1])
+    return df
 
 def interest_per_region(country_code):
     pytrends.build_payload(KW, CAT, TIMEFRAMES[2], country_code, GPROP)
@@ -65,6 +63,8 @@ def related_queries(country_code):
             print('There is not enough data')
         else:
             print(data[kw]['top'].head(9))
+            for index, row in data[kw]['top'].iterrows():
+                QUERY_ARR.append(row['query'])
 
         print(kw + ' rising queries: ')
         if data[kw]['rising'] is None:
@@ -77,6 +77,23 @@ def country_trends(country_code):
     data = pytrends.trending_searches(country_code)
     print(data)
 
+def request_window(search_arr):
+    request_arr = []
+    plt.figure()
+
+    for i in range(len(search_arr)):
+        request_arr.append(search_arr[i])
+        
+        if i % 5 == 0:
+            arrange_data(country_code, request_arr)
+            request_arr = []
+
+def plot_data(data):
+    plt.figure()
+    df.plot()
+    print("shit should be plotted")
+    plt.show()
+
 # for kw in KEYWORDS:
 #     KW.append(kw)
 #     over_time_interest = interest_over_time(country_code)
@@ -85,7 +102,12 @@ def country_trends(country_code):
 #     print(per_region_interest)
 #     KW.pop()
 
-# related_queries(country_code)
+related_queries(country_code)
 # country_trends(country)
-# plot_absolute_comparison(country_code)
-get_absolute_data(country_code)
+# print(QUERY_ARR)
+SEARCH_ARR = [*KEYWORDS, *QUERY_ARR]
+#plot_absolute_comparison(country_code, KEYWORDS)
+# get_absolute_data(country_code)
+request_window(SEARCH_ARR)
+plot_data(df)
+
